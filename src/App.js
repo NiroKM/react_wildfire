b8a3c730011a5afdc269ec6029fdbe93e191d4fb
Niroshan-Krishnamoorthy <niroshantrinity@gmail.com>
2020-12-20 00:41:13 +0530

    Api Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import Map from "./components/Map";
-
+import React, { useState, useEffect } from 'react'
+import { Loader } from "./components/Loader";
 
 function App() {
+  const [eventData,setEventData] = useState([]);
+  const [loading,setLoading] = useState(false);
+
+  // useEffect(()=>{
+  //   const fetchData = () => {
+  //     setLoading(true);
+  //     fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
+  //     .then(res=>res.json())
+  //     .then(data=>{
+  //       setEventData(data.events);
+  //     })
+  //     setLoading(false);
+  //   }
+  //   fetchData();
+  //   console.log(eventData)
+  // },[])
+
+  useEffect(()=>{
+    const fetchData = async()=>{
+      setLoading(true);
+      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events');
+      const {events} = await res.json();
+      setEventData(events);
+      setLoading(false);
+    }
+
+    fetchData();
+    console.log(eventData) 
+  },[])
+  
+  	
   return (
-    <div>
-      <Map/>
+    <div> 
+      {!loading ? <Map eventData={eventData}/> : <Loader/>}
     </div>
   );
 }
-
+  
 export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,28 +1,34 @@
-import React, { Component } from 'react';
+import React /*{ Component }*/ from 'react';
 import GoogleMapReact from 'google-map-react';
+import LocationMarker from './LocationMarker';
 
-class Map extends Component {
-    static defaultProps = {
-      center: {
-        lat: 36.7783,
-        lng: -119.4179
-      },
-      zoom: 7
-    };
-   
-    render() {
-      return (
-        // Important! Always set the container height explicitly
-        <div style={{ height: '100vh', width: '100%' }}>
+  const Map = ({center,zoom,eventData}) =>{
+      const wildfire = eventData.map(event=>{
+        if(event.categories[0].id===8){
+          return <LocationMarker lat={event.geometries[0].coordinates[1]} lng={event.geometries[0].coordinates[0]}/>
+        }
+      });
+      return(
+        <div className = "map">
           <GoogleMapReact
-            bootstrapURLKeys={{ key: /* Add Key Here */ }}
-            defaultCenter={this.props.center}
-            defaultZoom={this.props.zoom}
+            bootstrapURLKeys={{ key: /*Add you Api Here */}}
+             defaultCenter = {center}
+             defaultZoom = {zoom}
           >
+
+            {wildfire}
           </GoogleMapReact>
         </div>
       );
-    }
+    
   }
    
+  Map.defaultProps = {
+    center : {
+      lat : 38.5816,
+      lng : -121.4944
+    },
+    zoom: 7
+  }
+
   export default Map;
